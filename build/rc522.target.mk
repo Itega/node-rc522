9b81a2022316547459019ab00e87caa46e37fd29
Itega <julien.zolli1@gmail.com>
2018-10-05 16:47:44 +0200

    new build

--- build/rc522.target.mk
+++ build/rc522.target.mk
@@ -4,11 +4,15 @@ TOOLSET := target
 TARGET := rc522
 DEFS_Debug := \
 	'-DNODE_GYP_MODULE_NAME=rc522' \
+	'-DUSING_UV_SHARED=1' \
+	'-DUSING_V8_SHARED=1' \
+	'-DV8_DEPRECATION_WARNINGS=1' \
 	'-D_LARGEFILE_SOURCE' \
 	'-D_FILE_OFFSET_BITS=64' \
 	'-DBUILDING_NODE_EXTENSION' \
 	'-DDEBUG' \
-	'-D_DEBUG'
+	'-D_DEBUG' \
+	'-DV8_ENABLE_CHECKS'
 
 # Flags passed to all source files.
 CFLAGS_Debug := \
@@ -26,15 +30,23 @@ CFLAGS_C_Debug :=
 # Flags passed to only C++ files.
 CFLAGS_CC_Debug := \
 	-fno-rtti \
-	-fno-exceptions
+	-fno-exceptions \
+	-std=gnu++1y
 
 INCS_Debug := \
-	-I/home/pi/.node-gyp/0.12.6/src \
-	-I/home/pi/.node-gyp/0.12.6/deps/uv/include \
-	-I/home/pi/.node-gyp/0.12.6/deps/v8/include
+	-I/home/pi/.node-gyp/10.11.0/include/node \
+	-I/home/pi/.node-gyp/10.11.0/src \
+	-I/home/pi/.node-gyp/10.11.0/deps/openssl/config \
+	-I/home/pi/.node-gyp/10.11.0/deps/openssl/openssl/include \
+	-I/home/pi/.node-gyp/10.11.0/deps/uv/include \
+	-I/home/pi/.node-gyp/10.11.0/deps/zlib \
+	-I/home/pi/.node-gyp/10.11.0/deps/v8/include
 
 DEFS_Release := \
 	'-DNODE_GYP_MODULE_NAME=rc522' \
+	'-DUSING_UV_SHARED=1' \
+	'-DUSING_V8_SHARED=1' \
+	'-DV8_DEPRECATION_WARNINGS=1' \
 	'-D_LARGEFILE_SOURCE' \
 	'-D_FILE_OFFSET_BITS=64' \
 	'-DBUILDING_NODE_EXTENSION'
@@ -47,9 +59,6 @@ CFLAGS_Release := \
 	-Wextra \
 	-Wno-unused-parameter \
 	-O3 \
-	-ffunction-sections \
-	-fdata-sections \
-	-fno-tree-vrp \
 	-fno-omit-frame-pointer
 
 # Flags passed to only C files.
@@ -58,12 +67,17 @@ CFLAGS_C_Release :=
 # Flags passed to only C++ files.
 CFLAGS_CC_Release := \
 	-fno-rtti \
-	-fno-exceptions
+	-fno-exceptions \
+	-std=gnu++1y
 
 INCS_Release := \
-	-I/home/pi/.node-gyp/0.12.6/src \
-	-I/home/pi/.node-gyp/0.12.6/deps/uv/include \
-	-I/home/pi/.node-gyp/0.12.6/deps/v8/include
+	-I/home/pi/.node-gyp/10.11.0/include/node \
+	-I/home/pi/.node-gyp/10.11.0/src \
+	-I/home/pi/.node-gyp/10.11.0/deps/openssl/config \
+	-I/home/pi/.node-gyp/10.11.0/deps/openssl/openssl/include \
+	-I/home/pi/.node-gyp/10.11.0/deps/uv/include \
+	-I/home/pi/.node-gyp/10.11.0/deps/zlib \
+	-I/home/pi/.node-gyp/10.11.0/deps/v8/include
 
 OBJS := \
 	$(obj).target/$(TARGET)/src/rc522.o \
